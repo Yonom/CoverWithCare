24cf9fb3f0c5168c7a08aab3a6934dc1a97f2f65
Simon Farshid <Yonom@users.noreply.github.com>
2023-09-16 01:11:42 +0200

    Add an initial message

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import { nanoid } from '@/lib/utils'
 import { Chat } from '@/components/chat'
+import { Message } from 'ai'
 
 export const runtime = 'edge'
+
+const initialMessages: Message[] = [
+  {
+    id: nanoid(),
+    content: 'Hi, my house burned down!',
+    createdAt: new Date(),
+    role: 'assistant'
+  }
+]
 
 export default function IndexPage() {
   const id = nanoid()
 
-  return <Chat id={id} />
+  return <Chat id={id} initialMessages={initialMessages} />
 }
