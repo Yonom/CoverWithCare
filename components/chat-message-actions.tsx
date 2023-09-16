e1cedba3c919a216e7705153c89e29ef8745656d
Simon Farshid <Yonom@users.noreply.github.com>
2023-09-16 12:29:28 +0200

    Remove copy to clipboard

--- components/chat-message-actions.tsx
+++ components/chat-message-actions.tsx
@@ -18,26 +18,17 @@ export function ChatMessageActions({
   className,
   ...props
 }: ChatMessageActionsProps) {
-  const { isCopied, copyToClipboard } = useCopyToClipboard({ timeout: 2000 })
-
-  const onCopy = () => {
-    if (isCopied) return
-    copyToClipboard(message.content)
-  }
-
   return (
     <div
       className={cn(
-        'flex items-center justify-end transition-opacity group-hover:opacity-100 md:absolute md:-right-10 md:-top-2 md:opacity-0',
+        'flex items-center justify-end transition-opacity',
         className
       )}
       {...props}
     >
-      <Button variant="ghost" size="icon" onClick={onCopy}>
-        {isCopied ? <IconCheck /> : <IconCopy />}
-        <span className="sr-only">Copy message</span>
-      </Button>
-      <div className="border border-red">{coachMessage?.content}</div>
+      {!!coachMessage && (
+        <div className="border border-red-500 p-2">{coachMessage.content}</div>
+      )}
     </div>
   )
 }
