ceba4dffbc0d6b2b1085f64e6f786d6d00c37dcc
Pragya Tyagi <pragyatyagi@Pragyas-MacBook-Air.local>
2023-09-16 18:15:00 +0530

    merge conflicts

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -32,16 +32,7 @@ export function ChatMessage({
         className={cn('group relative mb-4 flex items-start md:-ml-12')}
         {...props}
       >
-        <div
-          className={cn(
-            'flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border shadow',
-            message.role === 'user'
-              ? 'bg-background'
-              : 'bg-background text-primary-foreground icon-radius'
-          )}
-        >
-            {message.role === 'user' ? <IconUser /> : <div/>}
-        </div>
+        {message.role === 'user' ? <IconUser /> : <div/>}
         <div
           className="flex-1 px-1 ml-4 space-y-2 overflow-hidden"
           style={{
