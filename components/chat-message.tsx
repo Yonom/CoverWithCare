7e4b17f665c360c8952d7242f142924c1a6847b9
Pragya Tyagi <pragyatyagi@Pragyas-MacBook-Air.local>
2023-09-16 15:54:03 +0530

    UI changes for chatbot

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -31,18 +31,18 @@ export function ChatMessage({
           'flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border shadow',
           message.role === 'user'
             ? 'bg-background'
-            : 'bg-primary text-primary-foreground'
+            : 'bg-background text-primary-foreground icon-radius'
         )}
       >
         {message.role === 'user' ? <IconUser /> : <IconOpenAI />}
       </div>
-      <div className="flex-1 px-1 ml-4 space-y-2 overflow-hidden">
+      <div className="flex-1 px-1 ml-4 space-y-2 overflow-hidden" style={{ "background" : '#A0D3EC', "padding": '10px 0px 10px 10px', "borderRadius": '0.5rem'}}>
         <MemoizedReactMarkdown
           className="prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0"
           remarkPlugins={[remarkGfm, remarkMath]}
           components={{
             p({ children }) {
-              return <p className="mb-2 last:mb-0">{children}</p>
+              return <p className="mb-2 last:mb-0" style={{color: '#FFFFFF'}}>{children}</p>
             },
             code({ node, inline, className, children, ...props }) {
               if (children.length) {
