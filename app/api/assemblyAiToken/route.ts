7c365aeda408979b18c83591b712cbe27e656abd
Simon Farshid <Yonom@users.noreply.github.com>
2023-09-17 02:54:41 +0200

    Fix env variable

--- app/api/assemblyAiToken/route.ts
+++ app/api/assemblyAiToken/route.ts
@@ -6,7 +6,7 @@ export async function GET() {
   const response = await fetch('https://api.assemblyai.com/v2/realtime/token', {
     method: 'POST',
     headers: {
-      Authorization: process.env.ASSSEMBLYAI_API_KEY!,
+      Authorization: process.env.ASSEMBLYAI_API_KEY!,
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ expires_in: 3600 })
